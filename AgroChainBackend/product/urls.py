3665e4f9b7666e4d1441b836318ab2bf89852b46
ramesh sah <rsah3798@gmail.com>
2024-11-28 21:50:54 +0545

    final

--- AgroChainBackend/product/urls.py
+++ AgroChainBackend/product/urls.py
@@ -5,12 +5,12 @@ from .views import CustomerProductAPIView,DistributorProductAPIView, RetailerPro
 
 
 urlpatterns = [
-    path('customer/products/', CustomerProductAPIView.as_view(), name='customer-product-list'),
-    path('customer/products/<int:pk>/', CustomerProductAPIView.as_view(), name='customer-product-detail'),
-    path('distributor/products/', DistributorProductAPIView.as_view(), name='distributor-product-list'),
-    path('distributor/products/<int:pk>/', DistributorProductAPIView.as_view(), name='distributor-product-detail'),
-    path('retailer/products/', RetailerProductAPIView.as_view(), name='retailer-product-list'),
-    path('retailer/products/<int:pk>/', RetailerProductAPIView.as_view(), name='retailer-product-detail'),
-    path('farmer/products/', FarmerProductAPIView.as_view(), name='farmer-product-list'),
-    path('farmer/products/<int:pk>/', FarmerProductAPIView.as_view(), name='farmer-product-detail'),
+    path('customer-products/', CustomerProductAPIView.as_view(), name='customer-product-list'),
+    path('customer-products/<int:pk>/', CustomerProductAPIView.as_view(), name='customer-product-detail'),
+    path('distributor-products/', DistributorProductAPIView.as_view(), name='distributor-product-list'),
+    path('distributor-products/<int:pk>/', DistributorProductAPIView.as_view(), name='distributor-product-detail'),
+    path('retailer-products/', RetailerProductAPIView.as_view(), name='retailer-product-list'),
+    path('retailer-products/<int:pk>/', RetailerProductAPIView.as_view(), name='retailer-product-detail'),
+    path('farmer-products/', FarmerProductAPIView.as_view(), name='farmer-product-list'),
+    path('farmer-products/<int:pk>/', FarmerProductAPIView.as_view(), name='farmer-product-detail'),
 ]
